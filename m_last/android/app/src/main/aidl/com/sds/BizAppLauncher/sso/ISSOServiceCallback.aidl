2089533b6a2f965bdd5316313589114fd09250e5
insunjang <ysjang6686@gmail.com>
2018-09-03 00:30:31 +0900

    swipe

--- m_last/android/app/src/main/aidl/com/sds/BizAppLauncher/sso/ISSOServiceCallback.aidl
+++
@@ -1,9 +0,0 @@
- 
- package com.sds.BizAppLauncher.sso;
- 
- interface ISSOServiceCallback {
-    /**
-     * Called when the service has a new state for you.
-     */
-    void signStateChanged(int value);
-}
--- m_last/app/notification_preferences/index.js
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
-// See License.txt for license information.
-
-import NotificationPreferences from './notification_preferences';
-export default NotificationPreferences;
